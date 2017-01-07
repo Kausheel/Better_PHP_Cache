c98825b34a170de54e4b0677659a6ad76de0a4d3
Kausheel Kumar <kausheelkumar@gmail.com>
2017-01-07 14:26:51 +1100

    First commit: create template of basic functions

---
+++ better_php_cache.php
@@ -0,0 +1,24 @@
+<?php
+    class Better_PHP_Cache
+    {
+        public function __construct()
+        {
+
+        }
+
+        public function add($entry_name, $entry_value, $time_to_live)
+        {
+
+        }
+
+        public function get($entry_name)
+        {
+
+        }
+
+        public function delete($entry_name)
+        {
+
+        }
+    }
+?>
