50fbbb8f268dc0f2bf98b93fec8df49b07afc48e
Kausheel Kumar <kausheelkumar@gmail.com>
2017-01-08 11:54:40 +1100

    Implement add() function with apc_store()

--- better_php_cache.php
+++ better_php_cache.php
@@ -8,7 +8,12 @@
 
         public function add($entry_name, $entry_value, $time_to_live)
         {
+            if(!($entry_name && $entry_value && $time_to_live))
+            {
+                return FALSE;
+            }
 
+            return apc_store($entry_name, $entry_value, $time_to_live);
         }
 
         public function get($entry_name)
