d0c2b680828dbe8a005baeae157ef81955ac3f3c
Kausheel Kumar <kausheelkumar@gmail.com>
2017-01-24 12:40:59 +1100

    refresh_entry_ttl() should only reset the ttl on non-expired entries

--- better_php_cache.php
+++ better_php_cache.php
@@ -135,13 +135,23 @@
             if($entry_in_filesystem == TRUE)
             {
                 $entry_value = $this->fetch_from_filesystem($entry_name);
-                return $this->store_in_filesystem($entry_name, $entry_value, $time_to_live);
+
+                if($entry_value)
+                {
+                    return $this->store_in_filesystem($entry_name, $entry_value, $time_to_live);
+                }
             }
             else
             {
                 $entry_value = apc_fetch($entry_name);
-                return apc_store($entry_name, $entry_value, $time_to_live);
+
+                if($entry_value)
+                {
+                    return apc_store($entry_name, $entry_value, $time_to_live);
+                }
             }
+
+            return FALSE;
         }
 
         //Remove existing cache statistics data.
